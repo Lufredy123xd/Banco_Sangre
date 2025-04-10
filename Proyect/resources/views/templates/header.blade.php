f93d07291267f3e10767b1dc32a7dfc8ae0fa8ae
Luciano Guerrero <106494315+Lufredy123xd@users.noreply.github.com>
2025-04-10 20:15:44 -0300

    Estructuramiento de datos + encabezado principal

---
+++ Proyect/resources/views/templates/header.blade.php
@@ -0,0 +1,22 @@
+<header class="header">
+    <div class="header__dropdown">
+        <button class="header__dropdown-button">
+            <img src="imgs/dropdown.png" alt="" class="header__dropdown-icon">
+        </button>
+        <nav class="header__nav">
+            <a href="" class="header__nav-link">Inicio</a>
+            <a href="" class="header__nav-link">Registrar</a>
+            <a href="" class="header__nav-link">Editar</a>
+            <a href="" class="header__nav-link">Eliminar</a>
+        </nav>
+    </div>
+    <div class="header__left">
+        <img src="imgs/vaccine_icon.png" alt="vaccine" class="header__left-icon">
+        <h1 class="header__left-title">BloodBank</h1>
+    </div>
+
+    <div class="header__right">
+        <h2 class="header__right-title">Usuario</h2>
+      <a href="#"><img src="imgs/user_icon.png" alt=""></a>
+    </div>
+</header>
--- Proyect/routes/web.php
+++ Proyect/routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+// routes/web.php
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq');
