b9d5b0a70fc3f6b9da10d68d7365d8d3de71b960
ValentinoVignolo <jesusmart392@gmail.com>
2025-05-21 19:11:35 -0300

    Read Only in donacion edit

--- Proyect/resources/views/donacion/edit.blade.php
+++ Proyect/resources/views/donacion/edit.blade.php
@@ -11,6 +11,12 @@
 
 <body>
     <div class="container mt-5">
+        @if (session('mensaje'))
+            <div class="alert alert-success">
+                {{ session('mensaje') }}
+            </div>
+        @endif
+
         <h1 class="mb-4 text-center">Editar Donación</h1>
         <form action="{{ url('/donacion/' . $donacion->id) }}" method="POST" class="p-4 border rounded shadow">
             {{ csrf_field() }}
@@ -18,27 +24,27 @@
 
             <div class="mb-3">
                 <label for="id_donante" class="form-label">Donante:</label>
-
                 @php
                     $donante = App\Models\Donante::find($donacion->id_donante);
                     $nombreCompleto = $donante->nombre . ' ' . $donante->apellido;
                 @endphp
-
                 <input type="hidden" id="id_donante" name="id_donante" value="{{ $donacion->id_donante }}">
-                <input value="{{ $nombreCompleto }}" class="form-control" required>
-                    
+                <input value="{{ $nombreCompleto }}" class="form-control" readonly>
+
             </div>
 
             <div class="mb-3">
                 <label for="fecha" class="form-label">Fecha:</label>
-                <input type="date" id="fecha" name="fecha" class="form-control" value="{{ $donacion->fecha }}" required>
+                <input type="date" id="fecha" name="fecha" class="form-control" value="{{ $donacion->fecha }}"
+                    required>
             </div>
 
             <div class="mb-3">
                 <label for="serologia" class="form-label">Serología:</label>
                 <select id="serologia" name="serologia" class="form-control" required>
                     @foreach (App\Enums\TipoSerologia::cases() as $tipo)
-                        <option value="{{ $tipo->value }}" {{ $donacion->serologia === $tipo->value ? 'selected' : '' }}>
+                        <option value="{{ $tipo->value }}"
+                            {{ $donacion->serologia === $tipo->value ? 'selected' : '' }}>
                             {{ $tipo->value }}
                         </option>
                     @endforeach
@@ -49,7 +55,8 @@
                 <label for="anticuerpos_irregulares" class="form-label">Anticuerpos Irregulares:</label>
                 <select id="anticuerpos_irregulares" name="anticuerpos_irregulares" class="form-control" required>
                     @foreach (App\Enums\TipoAnticuerposIrregulares::cases() as $tipo)
-                        <option value="{{ $tipo->value }}" {{ $donacion->anticuerpos_irregulares === $tipo->value ? 'selected' : '' }}>
+                        <option value="{{ $tipo->value }}"
+                            {{ $donacion->anticuerpos_irregulares === $tipo->value ? 'selected' : '' }}>
                             {{ $tipo->value }}
                         </option>
                     @endforeach
@@ -60,7 +67,8 @@
                 <label for="clase_donacion" class="form-label">Clase de Donación:</label>
                 <select id="clase_donacion" name="clase_donacion" class="form-control" required>
                     @foreach (App\Enums\TipoDonacion::cases() as $tipo)
-                        <option value="{{ $tipo->value }}" {{ $donacion->clase_donacion === $tipo->value ? 'selected' : '' }}>
+                        <option value="{{ $tipo->value }}"
+                            {{ $donacion->clase_donacion === $tipo->value ? 'selected' : '' }}>
                             {{ $tipo->value }}
                         </option>
                     @endforeach
