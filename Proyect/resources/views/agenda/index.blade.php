7e3baf263b010aad273d95cc736f8d93577953e2
Niko-44 <106493627+Niko-44@users.noreply.github.com>
2025-06-02 21:39:10 -0300

    cambio en la tabla

--- Proyect/resources/views/agenda/index.blade.php
+++ Proyect/resources/views/agenda/index.blade.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="table-responsive">
-                    <table class="table table-striped table-bordered" id="tablaAgendas">
+                    <table class="table table-striped" id="tablaAgendas">
                         <thead class="table-dark">
                             <tr>
                                 <th>#</th>
